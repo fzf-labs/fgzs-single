98199d56b398bbe7fabf8b29ef6ca192f15095de
付智斐 <382391525@qq.com>
2023-04-02 16:08:59 +0800

    refactor(pkg): pkg

--- internal/app/web/web.go
+++ internal/app/web/web.go
@@ -7,10 +7,9 @@ import (
 	"fgzs-single/internal/middleware"
 	"flag"
 	"fmt"
+	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/core/service"
-
-	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/rest"
 )
 
